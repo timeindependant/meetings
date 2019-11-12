2a3b615e9827f057b6d628c7d0f74d5aa164844f
hameed <vjhameed3@gmail.com>
2019-11-12 05:19:31 -0800

    fix

--- src/Defaults.js
+++ src/Defaults.js
@@ -1,64 +1,71 @@
-import Stop from './assets/stop.png'
-import Balance from './assets/balance.png'
-import Check from './assets/check.png'
-import Clown from './assets/clown.png'
-import Fact2 from './assets/Fact2.png'
-import Science from './assets/science.png'
+import Stop from "./assets/stop.png";
+import solution from "./assets/solution.png";
+import Check from "./assets/check.png";
+import agenda from "./assets/agenda.png";
+import summary from "./assets/summary.png";
+import idea from "./assets/idea.png";
+import balance from "./assets/balance.png";
 
-export const MARKER_SIZE = 20
+export const MARKER_SIZE = 20;
 
-export const DOWN_SCALE_FACTOR = 0.6
-export const LOST_PETAL_DOWN_SCALE_FACTOR = 1
+export const DOWN_SCALE_FACTOR = 0.6;
+export const LOST_PETAL_DOWN_SCALE_FACTOR = 1;
 
-export const MAGNIFY_SPEED = 600
-export const UNMAGNIFY_SPEED = 800
+export const MAGNIFY_SPEED = 600;
+export const UNMAGNIFY_SPEED = 800;
 
-export const NAVBAR_HEIGHT = 60
-export const SIDEBAR_WIDTH = 320
+export const NAVBAR_HEIGHT = 60;
+export const SIDEBAR_WIDTH = 320;
 
 export const FLAVORS = [
   {
-    name: 'Contradiction',
-    type: 'contra',
-    color: '#E74949',
+    name: "Contradiction",
+    type: "contra",
+    color: "#E74949",
     icon: Stop
   },
   {
-    name: 'Support',
-    type: 'pro',
-    color: '#36B37E',
+    name: "Support",
+    type: "pro",
+    color: "#36B37E",
     icon: Check
   },
   {
-    name: 'Neutral',
-    type: 'neutral',
-    color: '#7A869A',
-    icon: Balance
+    name: "Nuetral",
+    type: "pro",
+    color: "#36B37E",
+    icon: balance
+  },
+  {
+    name: "Solution",
+    type: "solution",
+    color: "#7A869A",
+    icon: solution
   },
 
   {
-    name: 'Science',
-    type: 'science',
-    color: '#6554C0',
-    icon: Science
+    name: "Idea",
+    type: "idea",
+    color: "#6554C0",
+    icon: idea
   },
   {
-    name: 'Joke',
-    type: 'joke',
-    color: '#ffAB00',
-    icon: Clown
+    name: "Agenda Topic",
+    type: "agenda",
+    color: "#ffAB00",
+    icon: agenda
   },
   {
-    name: 'Fact Checker',
-    type: 'fact check',
-    color: '#2684FF',
-    icon: Fact2,
+    name: "Summary",
+    type: "summary",
+    color: "#2684FF",
+    icon: summary,
     size: 41
   }
-]
+];
 
-export function getFlavor (type) {
+export function getFlavor(type) {
   return FLAVORS.find(element => {
-    return element.type === type
-  })
+    return element.type === type;
+  });
 }
