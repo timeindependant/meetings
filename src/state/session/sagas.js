3b635d668a30798de6b2a8fe4b803f91248f0b7d
PeterKey <p.korcsak@googlemail.com>
2020-01-23 18:56:15 +0100

    Added experimental JWT support

--- src/state/session/sagas.js
+++ src/state/session/sagas.js
@@ -1,32 +1,61 @@
 import { put, takeEvery, takeLatest } from 'redux-saga/effects'
+import jwtDecode from 'jwt-decode'
 import { LOGIN_LOADING, LOGIN_SUCCESS, LOGIN_ERROR,
   LOGIN_LINK_LOADING, LOGIN_LINK_SUCCESS, LOGIN_LINK_ERROR,
-  LOGOUT_ERROR, LOGOUT_LOADING, LOGOUT_SUCCESS } from './actions'
+  LOGOUT_ERROR, LOGOUT_LOADING } from './actions'
 
 import { fetchAsync } from '../helpers'
 
+const AUTH_API_URL = 'https://auth.serv.timz.io/auth/graphql'
+
 function * login (action) {
   try {
-    const response = yield fetchAsync(
-      () => fetch(
-        `${process.env.REACT_APP_SERVER_URL}/api/checkLogin`,
-        { credentials: 'include' }
-      ))
-    yield put({ type: LOGIN_SUCCESS, name: response.name, role: response.role, id: response.id })
+    if (action.token) {
+      yield fetchAsync(
+        () => fetch(
+          AUTH_API_URL,
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${action.token}`
+            },
+            body: JSON.stringify({ query: `{ verifyToken() { message }}` })
+          }
+        ))
+
+      const decoded = jwtDecode(action.token)
+      localStorage.setItem('token', action.token)
+      yield put({ type: LOGIN_SUCCESS, name: decoded.name, role: decoded.role, id: decoded.id })
+    } else {
+      const token = localStorage.getItem('token')
+
+      if (!token) {
+        throw Error('No Token found.')
+      }
+
+      yield fetchAsync(
+        () => fetch(
+          AUTH_API_URL,
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify({ query: `{ verifyToken() { message }}` })
+          }
+        ))
+
+      const decoded = jwtDecode(action.token)
+      localStorage.setItem('token', action.token)
+      yield put({ type: LOGIN_SUCCESS, name: decoded.name, role: decoded.role, id: decoded.id })
+    }
   } catch (e) {
     if (!action.token) {
       yield put({ type: LOGIN_ERROR, error: e.message })
     } else {
-      try {
-        const response = yield fetchAsync(
-          () => fetch(
-            `${process.env.REACT_APP_SERVER_URL}/api/login?token=${action.token}`,
-            { credentials: 'include' }
-          ))
-        yield put({ type: LOGIN_SUCCESS, token: true, name: response.name, role: response.role, id: response.id })
-      } catch (e) {
-        yield put({ type: LOGIN_ERROR, error: e.message, token: true })
-      }
+      yield put({ type: LOGIN_ERROR, error: e.message, token: true })
     }
   }
 }
@@ -39,14 +68,13 @@ function * getLoginLink (action) {
   try {
     yield fetchAsync(
       () => fetch(
-        `${process.env.REACT_APP_SERVER_URL}/api/requestLoginLink`,
+        AUTH_API_URL,
         {
-          credentials: 'include',
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
           },
-          body: JSON.stringify({ email: action.email })
+          body: JSON.stringify({ mutation: `{ login(email: ${action.email}) { message }}` })
         }
       ))
     yield put({ type: LOGIN_LINK_SUCCESS })
@@ -59,17 +87,21 @@ export function * loginLinkSaga () {
   yield takeLatest(LOGIN_LINK_LOADING, getLoginLink)
 }
 
-function * logout (action) {
+function * logout () {
   try {
     yield fetchAsync(
       () => fetch(
-        `${process.env.REACT_APP_SERVER_URL}/api/logout`,
+        AUTH_API_URL,
         {
-          credentials: 'include',
-          method: 'POST'
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ mutation: `{ logout() { message }}` })
         }
       ))
-    yield put({ type: LOGOUT_SUCCESS })
+    localStorage.removeItem('token')
+    location.reload()
   } catch (e) {
     yield put({ type: LOGOUT_ERROR, message: e.message })
   }
